7204adc3b828247091a628f0a4d47dbc50454058
Mkonrad-afk <s29778@pjwstk.edu.pl>
2025-05-06 15:12:08 +0200

    init

--- Tutorial8/Models/DTOs/TripDTO.cs
+++ Tutorial8/Models/DTOs/TripDTO.cs
@@ -12,3 +12,13 @@ public class CountryDTO
     public int Id { get; set;  }
     public string Name { get; set; }
 }
+
+public class ClientDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Pesel { get; set; }
+}
